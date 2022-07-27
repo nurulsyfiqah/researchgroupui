c2c01adb894a6845962e791a57dfd5b6d2d87684
nurulsyfiqah <nurulsyfiqah25@gmail.com>
2022-07-27 17:51:13 +0800

    update service ui url

--- src/Service/serviceui.js
+++ src/Service/serviceui.js
@@ -1,2 +1,7 @@
-const base_url = "http://localhost:3000"
+// localhost
+// const base_url = "http://localhost:3000"
+// export default base_url
+
+// server
+const base_url = "https://researchgroupui.herokuapp.com"
 export default base_url
