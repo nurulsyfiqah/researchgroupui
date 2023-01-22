77af68fcab79077661aaff4f6fa93c27ec71c091
nurulsyfiqah <nurulsyfiqah25@gmail.com>
2023-01-22 21:30:25 +0800

    change ui

--- src/Service/serviceui.js
+++ src/Service/serviceui.js
@@ -2,6 +2,12 @@
 // const base_url = "http://localhost:3000"
 // export default base_url
 
+let base_url;
+if (process.env.IS_HEROKU === 'true') {
+    base_url = "https://researchgroupui.herokuapp.com"
+} else {
+    base_url =  "http://localhost:3000"
+}
 // server
-const base_url = "https://researchgroupui.herokuapp.com"
+// const base_url = "https://researchgroupui.herokuapp.com"
 export default base_url
