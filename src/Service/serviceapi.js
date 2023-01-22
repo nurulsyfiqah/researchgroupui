000d301973b367671789fe457eb6cf11c1c89624
nurulsyfiqah <nurulsyfiqah25@gmail.com>
2023-01-22 21:29:04 +0800

    change api path

--- src/Service/serviceapi.js
+++ src/Service/serviceapi.js
@@ -1,5 +1,12 @@
 // const base_url = "http://localhost:8080"
-const base_url = "https://resgm.herokuapp.com"
+// const base_url = "https://resgm.herokuapp.com"
+
+let base_url;
+if (process.env.IS_HEROKU === 'true') {
+    base_url = "https://resgm.herokuapp.com"
+} else {
+    base_url =  "http://localhost:8080"
+}
 
 // const upload_url = "http://localhost:8081/Documents/FYP workspace/upload/"
 
